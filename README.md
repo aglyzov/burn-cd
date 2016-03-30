0727a4db62fd6288b3d19d3acec345473d452538
Ronald Farrer <canutethegreat@gmail.com>
2016-03-30 12:15:56 -0700

    Added a short example .burn-cd.conf as dotburn-cd.conf. Added TODO from renamed todo.txt. Split changelog into its own file. Removed mercurial files. Moved scripts other than burn-cd into tools/. Renamed todo.txt to TODO. Removed burn-cd.py symlink. Moved test-colors.py into tools. New ebuild for 1.8.1.

---
+++ README.md
@@ -0,0 +1,1 @@
+Burn-CD is a console-based frontend for the cdrtools/cdrkit and the dvd+rw-tools for easy burning of single & multi session CD/DVD discs.
--- install.sh
+++ install.sh
@@ -1,6 +1,9 @@
 #!/bin/sh
 
-DST=/usr/local/bin
+BIN=/usr/local/bin
+DOC=/usr/local/share/burn-cd
 
-cp -v burn-cd burn-audio burn-iso blank-cdrw "$DST"
+cp -v burn-cd "$BIN"
 
+mkdir ${DOC}
+cp -v Changelog dotburn-cd.conf ${DOC}
